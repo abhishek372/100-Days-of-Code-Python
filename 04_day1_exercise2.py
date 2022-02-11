f3dc45e8d1adcf189762baa778d893bb3a3768df
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-12 00:30:14 +0530

    Delete 04_day1_exercise2.py

--- 04_day1_exercise2.py
+++
@@ -1,4 +0,0 @@
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the '+' sign")
-print('e.g print("Hello " + "World")')
-print("New line can be created with a backslash and n.")
