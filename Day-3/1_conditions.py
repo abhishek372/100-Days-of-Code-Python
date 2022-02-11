5f9f7d3353f95bbc2c73837ea465d73100e6dd93
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-12 00:36:59 +0530

    Create 1_conditions.py

---
+++ Day-3/1_conditions.py
@@ -0,0 +1,9 @@
+# Conditional Operators
+# > , < , >= , <= , == , !=
+
+water_level = int(input("Enter the water level: "))
+
+if water_level>80:
+    print("Drain water")
+else:
+    print("Continue filling")
