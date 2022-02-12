7cf61709d8a360610b000f4b62f17b08fb77ffce
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-13 00:29:11 +0530

    Update 11_Project3_Treasure_Island.py

--- Day-3/11_Project3_Treasure_Island.py
+++ Day-3/11_Project3_Treasure_Island.py
@@ -32,7 +32,7 @@ print("Your mission is to find the treasure.")
 cross_road_decision = input("You're at a cross road. Where do you want to go? Type \"left\" or \"right\" \n").lower()
 
 if cross_road_decision=="left":
-    wait_or_swim = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" ti wait for a boat. Type \"swim\" to swim across.\n").lower()
+    wait_or_swim = input("You come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
     if wait_or_swim == "wait":
         colour = input("You arrive at the island unharmed. There is a house with 3 doors. One is red, one is yellow and one blue. Which colour do you choose?\n").lower()
         if colour=="yellow":
