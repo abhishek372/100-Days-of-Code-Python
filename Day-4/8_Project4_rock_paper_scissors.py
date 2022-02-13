552e9433cc3af8e7ba36d526a5ac81551763b14e
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-14 00:11:13 +0530

    Update 8_Project4_rock_paper_scissors.py

--- Day-4/8_Project4_rock_paper_scissors.py
+++ Day-4/8_Project4_rock_paper_scissors.py
@@ -29,60 +29,29 @@ scissors = '''
 ---.__(___)
 '''
 
-choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-choice = int(choice)
-elements = [rock, paper, scissors]
+game_elements = [rock, paper, scissors]
 
-computer_chosen = random.randint(0, len(elements)-1)
+computer_chosen = random.randint(0, len(game_elements)-1)
 
-# print(choice, computer_chosen)
+print(game_elements[choice])
+print("Computer chose: \n" ,game_elements[computer_chosen])
 
-if (choice==0) and (computer_chosen==0):
-    print(rock)
-    print("\nComputer chose:\n")
-    print(rock)
+if (choice == computer_chosen):
     print("Match drawn")
 elif (choice == 0) and (computer_chosen == 1):
-    print(elements[0])
-    print("\nComputer chose:\n")
-    print(elements[1])
-    print("Computer wins")
+    print("You lose!")
 elif (choice == 0) and (computer_chosen == 2):
-    print(elements[0])
-    print("\nComputer chose:\n")
-    print(elements[2])
     print("You win")
 elif (choice == 1) and (computer_chosen == 0):
-    print(elements[1])
-    print("\nComputer chose:\n")
-    print(elements[0])
-    print("You wins")
-elif (choice == 1) and (computer_chosen == 1):
-    print(elements[1])
-    print("\nComputer chose:\n")
-    print(elements[1])
-    print("Match drawn")
+    print("You win")
 elif (choice == 1) and (computer_chosen == 2):
-    print(elements[1])
-    print("\nComputer chose:\n")
-    print(elements[2])
-    print("Computer wins")
+    print("You lose!")
 elif (choice == 2) and (computer_chosen == 0):
-    print(elements[2])
-    print("\nComputer chose:\n")
-    print(elements[0])
-    print("Computer wins")
+    print("You lose!")
 elif (choice == 2) and (computer_chosen == 1):
-    print(elements[2])
-    print("\nComputer chose:\n")
-    print(elements[1])
-    print("You wins")
-elif (choice == 2) and (computer_chosen == 2):
-    print(elements[2])
-    print("\nComputer chose:\n")
-    print(elements[2])
-    print("Match drawn")
+    print("You win")
 
 
 
