65450edd94a2acd590aacf3a20e976978de2625f
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-13 23:45:38 +0530

    Create 1_random.py

---
+++ Day-4/1_random.py
@@ -0,0 +1,17 @@
+# Generating random numbers
+
+# Importing random module to generate random numbers
+import random
+
+random_integer = random.randint(1, 20)
+print(random_integer)
+
+# Importing mymodule file
+# import mymodule
+# print(mymodule.pi)
+
+random_float = random.random()
+print(random_float)
+
+# Floating Random numbers from 0 to 5(excluding)
+print(random_float*5)
