40f8cd9817b7e15ac9edf7c452cf96c4022c0683
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-24 10:48:49 +0530

    Add files via upload

---
+++ Day-6/1_Alone_challenge.py
@@ -0,0 +1,21 @@
+# Link - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+# turn_left(), move() are given functions at website to move.
+
+def turn_around():
+    turn_left()
+    turn_left()
+    turn_left()
+  
+
+# Draw a square
+turn_left()
+move()
+turn_around()
+move()
+turn_around()
+move()
+turn_around()
+move()
+
+
---
+++ Day-6/3_Indentation.py
@@ -0,0 +1,8 @@
+# To determine the codeblocks of a function ,
+# statements inside the function should be indented
+
+for _ in range(6):
+    print(_)                    # this print statement is inside the loop
+
+print("Outside the loop")       # this print statement is outside the loop
+
