8364f259ca8e777fceb736bd2fd8622c6ab7c100
Abhishek Kumar <72251089+abhishek372@users.noreply.github.com>
2022-02-24 10:52:07 +0530

    Update and rename 2_area_calculator_ex1.py to 2_Area_calculator.py

--- Day-8/2_Area_calculator.py
+++ Day-8/2_Area_calculator.py
@@ -1,14 +1,17 @@
 # Program to calculate the number of cans required to paint a wall
 
+# Inputs
 wall_h = int(input("Height of wall: "))
 wall_w = int(input("Width of wall: "))
 coverage_per_can = 5
 
 number_of_cans = 0
 
+# Paint_calculator
 def paint_calc(height, width, cover):
     number_of_cans = round((height*width) / cover)
     print(f"You will need {number_of_cans} cans of paint.")
 
+# Calling the function
 paint_calc(height=wall_h, width=wall_w, cover=coverage_per_can)
 
